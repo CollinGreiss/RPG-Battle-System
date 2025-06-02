d7ffd452b7f8d78057f48c7dca7081a462098081
Collin Greiss <123586114+CollinGreiss@users.noreply.github.com>
2025-06-02 13:56:11 -0600

    Builder Pattern
    Created simple character as an example
    Created builder interface for different character types
    Created mage and warrior builders for full example
    Created CharacterDirector for easy creation

---
+++ RPG Battle System/Models/WarriorBuilder.cs
@@ -0,0 +1,17 @@
+﻿class WarriorBuilder : ICharacterBuilder {
+
+    private Character _character = new Character();
+
+    public void SetName( string name ) => _character.Name = name;
+    public void SetClass() => _character.Class = "Warrior";
+    public void SetStats() {
+
+        _character.Health = 150;
+        _character.Attack = 25;
+        _character.Defense = 10;
+
+    }
+
+    public Character GetResult() => _character;
+
+}
